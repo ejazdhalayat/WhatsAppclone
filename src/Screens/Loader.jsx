56d9c8054166a4afa448285bf12bf76fa9f68b91
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2022-12-22 21:28:46 +0530

    Loader

--- src/Screens/Loader.jsx
+++ src/Screens/Loader.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div>
-        <div>
-            <img />
-            <p>Loading</p>
+    <div className=' flex h-screen justify-center items-center  bg-black'>
+        <div className='relative'>
+            <img src='https://cdn.dribbble.com/users/1788187/screenshots/5095383/media/992c486d46e357bc5f5cb08397f1bcf9.gif'/>
+            
         </div>
     </div>
   )
