85bca371dc9cd7fc64d2992f2469a26b06469cb3
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2022-12-27 23:44:14 +0530

    ChatScreen

--- src/Components/ChatScreen.jsx
+++ src/Components/ChatScreen.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatHead from './SubComponent/ChatHead'
 import ChatInput from './SubComponent/ChatInput'
 import ChatScroll from './SubComponent/ChatScroll'
 
 function ChatScreen({db, user, ...props}) {
+  const [messageArray, setMessageArray] = useState([]) ;
+
+
   return (
     <div className='bg-blue-600 relative col-span-4 h-screen'>
         <div className='bg-gray-300 w-full absolute z-10 top-0'>
           <ChatHead p2={props.p1} />
         </div>
         <div className="bg-[url('https://st2.depositphotos.com/1963019/9464/v/450/depositphotos_94649040-stock-illustration-vector-doodle-seamless-pattern-with.jpg')]  pt-16 h-full overflow-y-scroll">
-          <ChatScroll />
+          <ChatScroll db={db} user={user} msgSetter={setMessageArray} />
           </div>
         <div className='bg-white w-full  absolute bottom-0'>
-          <ChatInput db={db} user={user} />
+          <ChatInput db={db} user={user} msgSetter={setMessageArray} />
           </div>
 
       </div>
