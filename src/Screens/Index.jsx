e623afb7e1eb785e5bf053ae3d9849a5a1cb6779
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-22 01:03:54 +0530

    Loader

--- src/Screens/Index.jsx
+++ src/Screens/Index.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Index() {
-    const[contact, setContact] = useState("anmol")
-    
-   
-    function me(){
-        setContact("priya")
-        console.log(contact)
-    }
+  const [contact, setContact] = useState("anmol");
+
+  function me() {
+    setContact("priya");
+    console.log(contact);
+  }
   return (
-      <div>
-        <p>{contact}</p>  
-        <button  onClick={me}>Click Me</button>
-      </div>
-  )
+    <div>
+      <p>{contact}</p>
+      <button onClick={me}>Click Me</button>
+    </div>
+  );
 }
 
-export default Index
+export default Index;
