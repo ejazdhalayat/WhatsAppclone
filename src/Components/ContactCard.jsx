31fbfc5e6c2708d601b21094fa5c63fc830a15a2
Ejaz-Ul-Huque <ejazdhalayat45@gmail.com>
2023-01-22 01:00:47 +0530

    CH

--- src/Components/ContactCard.jsx
+++ src/Components/ContactCard.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
+import React from "react";
 
-function ContactCard({showImg ,showName, ...props }) { //we can give any parameter. Developers call props in react
-//   console.log(props)
-    return (
-    <div className=' text-gray-500 border-slate-700 group cursor-pointer hover:bg-gradient-to-tr from-purple-500 to-yellow-400 border-y py-3 text-sm'>
-        <div className=' flex justify-between mx-4'>
-            <div className='flex space-x-2 items-center'>
-            <img className='rounded-full h-9 w-9' 
-            src={showImg} />
+function ContactCard({ showImg, showName, ...props }) {
+  //we can give any parameter. Developers call props in react
+  //   console.log(props)
+  return (
+    <div className=" text-gray-500 border-slate-700 group cursor-pointer hover:bg-gradient-to-tr from-purple-500 to-yellow-400 border-y py-3 text-sm">
+      <div className=" flex justify-between mx-4">
+        <div className="flex space-x-2 items-center">
+          <img className="rounded-full h-9 w-9" src={showImg} />
 
-                <div>
-                    <p className='text-white group-hover:font-bold group-hover:text-black text-base'>{showName}</p>
+          <div>
+            <p className="text-white group-hover:font-bold group-hover:text-black text-base">
+              {showName}
+            </p>
 
-                    
-                    <p className='text-gray-400 group-hover:text-black text-sm'>Your message was deleted</p>
-
-                </div>
-
-               
-
-            </div>
-
-            <div>
-                <p className='text-xs'>12:00</p>
-            </div>
-
+            <p className="text-gray-400 group-hover:text-black text-sm">
+              Your message was deleted
+            </p>
+          </div>
         </div>
 
+        <div>
+          <p className="text-xs">12:00</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactCard
+export default ContactCard;
--- src/Components/SubComponent/ChatHead.jsx
+++ src/Components/SubComponent/ChatHead.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from "react";
 
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import SearchIcon from '@mui/icons-material/Search';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
 
 function ChatHead(props) {
   return (
     <div>
-          <div className='flex justify-between p-2 px-4 items-center
-       bg-slate-700 '>
-        <div className='flex items-center space-x-3'>
-        <img className='rounded-full h-10 w-10' alt='' src={props.p2?.photoURL} />
-        <div>
-            <p className='text-gray-300 font-semibold text-lg '>{props.p2?.displayName}</p>
-            <p className='text-gray-300 font-semibold text-xs leading-3'>Online</p>
+      <div
+        className="flex justify-between p-2 px-4 items-center
+       bg-slate-700 "
+      >
+        <div className="flex items-center space-x-3">
+          <img
+            className="rounded-full h-10 w-10"
+            alt=""
+            src={props.p2?.photoURL}
+          />
+          <div>
+            <p className="text-gray-300 font-semibold text-lg ">
+              {props.p2?.displayName}
+            </p>
+            <p className="text-gray-300 font-semibold text-xs leading-3">
+              Online
+            </p>
+          </div>
         </div>
 
+        <div className="flex text-gray-300 text-lg space-x-3">
+          <SearchIcon fontSize="inherit" />
+          <MoreVertIcon fontSize="inherit" />
         </div>
-
-        <div className='flex text-gray-300 text-lg space-x-3'>
-        <SearchIcon fontSize='inherit' />
-        <MoreVertIcon fontSize='inherit' />
-        </div>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default ChatHead
+export default ChatHead;
